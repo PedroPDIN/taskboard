02cbde7a3055439434b3f9557a07ea36444d0e9f
Pedro Lima <pedro.oficial1969@gmail.com>
2022-10-04 18:14:20 -0300

    feat(APIs e styles): adiciona mas algumas apis de criação de board e tasks, e também a funcionalidade de alteração de estado da task e estilização da aplicação com o bootstrap.

--- taskboard/urls.py
+++ taskboard/urls.py
@@ -1,6 +1,6 @@
 from rest_framework import routers # cria a rota
 from django.urls import path, include
-from .views import BoardViewSets, TaskViewSets, index
+from .views import BoardViewSets, TaskViewSets, index, board_page, new_board, new_task, edit_task
 
 
 router = routers.DefaultRouter()
@@ -10,5 +10,9 @@ router.register(r'tasks', TaskViewSets)
 
 urlpatterns = [
     path('', index, name='homepage'),
+    path('<int:board_id>', board_page, name='boardpage'),
+    path('new-board', new_board, name='newboard'),
+    path('new-task', new_task, name='newtask'),
+    path('edit-task', edit_task, name='edittask'),
     path('api/', include(router.urls))
 ]
--- taskboard/views.py
+++ taskboard/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import BoardForm, TaskForm
 from .models import Board, Task
 from .serializer import BoardSerializer, TaskSerializer
 from rest_framework import viewsets
@@ -8,11 +9,51 @@ from rest_framework.decorators import action
 
 def index(request):
     context = {
-      'minha_chave': Board.objects.all() 
+      'boards': Board.objects.all() 
       }
 
     return render(request, 'index.html', context)
 
+
+def board_page(request, board_id: int):
+    board = get_object_or_404(Board, id=board_id)
+
+    context ={
+      'board': board,
+      'pending_tasks': Task.objects.all().filter(board_id=board_id).filter(status="Pending"),
+      'doing_tasks': Task.objects.all().filter(board_id=board_id).filter(status="Doing"),
+      'done_tasks': Task.objects.all().filter(board_id=board_id).filter(status="Done"),
+      'status_list': Task.status_list
+    }
+
+    return render(request, 'board.html', context)
+
+
+def new_board(request):
+    form = BoardForm(request.POST)
+    if form.is_valid():
+        form.save(commit=True) # "commit" caso não queira alterar nenhuma informação apenas salvar direto no banco de dados
+
+    return redirect('homepage')
+
+
+def new_task(request):
+    form = TaskForm(request.POST)
+    if form.is_valid():
+        form.save(commit=True)
+        return redirect('boardpage', board_id=request.POST['board_id'])
+    
+    return redirect('homepage')
+
+
+def edit_task(request):
+    id = int(request.POST["id"])
+    task = get_object_or_404(Task, id=id)
+    task.status = request.POST['status']
+    task.save()
+
+    return redirect('boardpage', board_id=task.board_id.id)
+    
 
 class BoardViewSets(viewsets.ModelViewSet):
     queryset = Board.objects.all()
